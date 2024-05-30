152e590f84bacc9003eb876389d2a1ce4d5e6f2f
Joshua Vaughan <josh.vaughan@instantteams.com>
2024-05-29 19:52:26 -0500

    Updated whole portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar';
+import Intro from './components/Intro';
+import Projects from './components/Projects';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <NavBar />
+      <Intro />
+      <Projects />
+      <About />
+      <Contact />
+      <Footer />
+    </main>
   );
 }
+
 
 export default App;
